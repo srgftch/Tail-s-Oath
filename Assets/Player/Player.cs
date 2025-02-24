181fc513064c3dace59fd6c150f2ba1c52987087
Srg_ftch <sergeyreck@mail.ru>
2025-02-24 23:48:41 +0400

    Анимации движения персонажа

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -6,8 +6,11 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField]
     private float speed = 3f;
     private Vector2 move;
+    [SerializeField]
+    private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,6 +22,16 @@ public class Player : MonoBehaviour
     [ContextMenu("Move")]
     public void Move(InputAction.CallbackContext context)
     {
+
+        animator.SetBool("isMoving", true);
+        if (context.canceled)
+        {
+            animator.SetBool("isMoving", false);
+            animator.SetFloat("lastX", move.x);
+            animator.SetFloat("lastY", move.y);
+        }
         move = context.ReadValue<Vector2>();
+        animator.SetFloat("inputX", move.x);
+        animator.SetFloat("inputY", move.y);
     }
 }
